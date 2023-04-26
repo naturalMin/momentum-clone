9ecadf64bc1da7955c4359661f8675aa40fadd38
jungmin kim <ketty0611@gmail.com>
2023-04-26 10:54:37 +0900

    clock second remove

--- script/clock.js
+++ script/clock.js
@@ -3,9 +3,8 @@ const clock = document.querySelector("#clock");
 function handleClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+    const minutes = String(date.getMinutes()).padStart(2, "0");    
+    clock.innerHTML = `${hours}:${minutes}`;
 }
 handleClock();
 setInterval(handleClock, 1000);
