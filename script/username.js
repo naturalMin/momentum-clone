91b0beb77fafbf8e70a63be8573065f5236ee355
jungmin kim <ketty0611@gmail.com>
2023-04-26 11:21:54 +0900

    update username login

--- script/username.js
+++ script/username.js
@@ -8,21 +8,19 @@ const USERNAME_KEY = "username";
 
 function loginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME); //로그인 후 login form 숨김
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    welcome.innerHTML = `Hi. ${username}. Have a nice today:)`;
+    welcome.innerHTML = `Hi, ${username}. Have a nice today:)`;
     welcome.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if (savedUsername === null) { //유저정보가 없다면,
-    //show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME); //login form 숨김처리 해제
+if (savedUsername === null) {     
+    loginForm.classList.remove(HIDDEN_CLASSNAME); 
     loginForm.addEventListener("submit", loginSubmit);
-} else {
-    //show the greeting
-    welcome.innerHTML = `Hi. ${savedUsername}. Have a nice today:)`;
+} else {    
+    welcome.innerHTML = `Hi, ${savedUsername}. Have a nice today:)`;
     welcome.classList.remove(HIDDEN_CLASSNAME);
 }
